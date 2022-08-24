c93e9e217a6ce5a2ce672431665e5902fbc9afb9
Genival Melo Junior <genivaldemelojr@gmail.com>
2022-08-23 23:06:05 -0300

    verify if email is already in use

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A users register and login system.
 
 [X] It needs to be possible to create a new user
-[ ] If the user already exists (verify email), show a "user already exists" message and don't create a new one
+[X] If the user already exists (verify email), show a "email already in use" message and don't create a new one
 [ ] It needs to hash the password of the user before saving in database
 [ ] It needs to be possible to get a list of names of all users registred
 [ ] It needs to be possible to update a user's info
--- src/controllers/CreateUsersController.ts
+++ src/controllers/CreateUsersController.ts
@@ -5,10 +5,18 @@ export class CreateUsersController{
     async create(req:Request, res: Response){
         const {name, email, password} = req.body;
 
-        const user = await prismaClient.users.create({data:{
-            name, email, password
+        const userAlreadyExists = await prismaClient.users.findFirst({where:{
+            email: email
         }});
 
-        return res.status(201).json({message: "user " + user.name + " created succesfuly"});
+        if(!userAlreadyExists){    
+            const user = await prismaClient.users.create({data:{
+                name, email, password
+            }});
+
+            return res.status(201).json({message: "user " + user.name + " created succesfuly"});
+        }
+
+        return res.status(409).json({message: "email already in use"})
     }
 }
