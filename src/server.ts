94cc24661621c94562b1184b8a5d377382ba4e7a
Genival Melo Junior <genivaldemelojr@gmail.com>
2022-08-23 22:50:41 -0300

    routes file and PrismaClient

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express, {json} from "express";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(json());
+app.use(routes);
 
 app.listen(3000, ()=>{
     console.log("server is running");
