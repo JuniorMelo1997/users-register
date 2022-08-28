05c992eae880517a80f01776db7fb8af8722c861
Genival Melo Junior <genivaldemelojr@gmail.com>
2022-08-27 23:42:27 -0300

    user can update/change only his own account

--- src/controllers/UpdateUserController.ts
+++ src/controllers/UpdateUserController.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 export class UpdateUserController{
     async updateUser(req: Request, res: Response){
         let {id, name, email, password} = req.body;
+        const {tokenId} = res.locals;
 
         const userExists = await prismaClient.users.findFirst({where:{
             id: id
@@ -14,9 +15,15 @@ export class UpdateUserController{
             return res.status(404).json({message: "no user found"});
         }
 
+        const verify = tokenId === id;
+
+        if(!verify){
+            return res.status(401).json({message: "you have no permission to change this user"});
+        }
+
         password = await bcrypt.hash(password, 10);
         
-        const updatedUser = await prismaClient.users.update({where: {
+        await prismaClient.users.update({where: {
             id: id
         }, data:{
             name, email, password
